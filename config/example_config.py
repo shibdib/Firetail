85d84ff9e61f128c7bbc639523d6f82028d9b769
shibdib <supernfadmin@gmail.com>
2017-12-11 16:41:00 -0500

    config comments

--- config/example_config.py
+++ config/example_config.py
@@ -7,19 +7,20 @@ authUrl = 'http://keepstar.shibdib.info'
 welcomeMessageEnabled = False  # Set to true if you'd like the message below to be sent to new users
 welcomeMessage = '**Welcome to the server!**\n \nTo get roles type !auth to get a link to the authing system.\nIf you ' \
                  'want more information regarding other plugins type !help'
+
 # Uncomment plugins to enable
 # On Message Plugins
 messagePlugins = (
-    'test',
-    'price',
+    'test',  # Test plugin please ignore
+    'price',  # Price check plugin
     #  'char'
 )
 # On Tick Plugins
 tickPlugins = (
-    #  'killmails'
+    #  'killmails'  #Killmail posting plugin
 )
 
-# Auto Responses
+# Auto Responses - Add more with the format 'trigger': 'Auto response'
 autoResponse = {
     'auth': 'To get roles on this server visit: ' + authUrl,
     'help': 'To get assistance with a plugin type !plugin help, currently active plugins are ' + str(messagePlugins)
@@ -27,11 +28,11 @@ autoResponse = {
 
 # Killmail Plugin Settings
 killmail = {  # add or remove groups as needed, groups must have unique names.
-    'bigKills': True,
-    'bigKillsValue': 1000000000,
-    'bigKillsChannel': '389827425581662226',
+    'bigKills': True,  # Enable the sharing of eve wide big kills
+    'bigKillsValue': 1000000000,  # Big kill ISK threshold
+    'bigKillsChannel': '389827425581662226',  # Channel big kills are posted to
     'killmailGroups': {
-        'group1': {
+        'group1': {  # feel free to add additional groups, be sure that each group has a unique name
             'id': '498125261',  # Corp/Alliance ID
             'channelId': '244061582496104448',  # Channel ID
             'lossMails': True  # Show Loss Mails
