815fca22362361d3c8c534fac99e11984470f3b1
shibdib <supernfadmin@gmail.com>
2017-12-11 07:51:07 -0500

    update price to use embed format

--- plugins/price.py
+++ plugins/price.py
@@ -1,4 +1,5 @@
 from lib import esi
+import discord
 
 
 async def run(client, logger, config, message):
@@ -8,23 +9,19 @@ async def run(client, logger, config, message):
         msg = item + " was not found, are you sure it's an item?".format(message)
         await client.send_message(message.channel, msg)
     else:
+        typeid = await esi.item_id(item)
         buymax = '{:,}'.format(float(data['buy']['max']))
         buymin = '{:,}'.format(float(data['buy']['min']))
         buyavg = '{:,}'.format(float(data['buy']['weightedAverage']))
         sellmax = '{:,}'.format(float(data['sell']['max']))
         sellmin = '{:,}'.format(float(data['sell']['min']))
         sellavg = '{:,}'.format(float(data['sell']['weightedAverage']))
-        raw = """System: **Jita **
-Item: **""" + item.capitalize() + """** 
-
-**Buy:** ```Low: """ + buymin + """ 
-Avg: """ + buyavg + """ 
-High: """ + buymax + """``` 
-**Sell:** ```Low: """ + sellmin + """ 
-Avg: """ + sellavg + """ 
-High: """ + sellmax + """```"""
-        msg = raw.format(message)
-        await client.send_message(message.channel, msg)
+        em = discord.Embed(title=item.title(), description="Price information from Jita 4-4", url="https://market.fuzzwork.co.uk/type/" + str(typeid) + "/", colour=0xDEADBF)
+        em.set_footer(icon_url=client.user.default_avatar_url, text="Provided Via Firetail Bot")
+        em.set_thumbnail(url="https://image.eveonline.com/Type/" + str(typeid) + "_64.png")
+        em.add_field(name="Buy", value="Low: " + buymin + " \nAvg: " + buyavg + " \nHigh: " + buymax + " \n ", inline=True)
+        em.add_field(name="Sell", value="Low: " + sellmin + " \nAvg: " + sellavg + " \nHigh: " + sellmax + " \n ", inline=True)
+        await client.send_message(message.channel, embed=em)
 
 
 async def helptext(client, logger, config, message):
