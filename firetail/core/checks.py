c6d3fc0327d4b945c9125ec0943129896f10632a
shibdib <supernfadmin@gmail.com>
2018-01-15 22:58:07 -0500

    fix for help being insta rate limit

--- firetail/core/checks.py
+++ firetail/core/checks.py
@@ -22,6 +22,8 @@ async def check_is_mod(ctx):
 
 
 async def check_spam(ctx):
+    if '!help' in ctx.message.content:
+        return True
     spam_list = ctx.bot.bot_users
     spam_list_length = len(spam_list)
     if spam_list_length >= 10:
