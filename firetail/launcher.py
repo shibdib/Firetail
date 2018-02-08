8565f9c3594141cae3adf33bef2fca9bdac7b080
scragly <29337040+scragly@users.noreply.github.com>
2018-02-09 06:44:34 +1000

    support config dir cli arg

--- firetail/launcher.py
+++ firetail/launcher.py
@@ -1,16 +1,29 @@
 import sys
+import os
 import argparse
 import subprocess
 
+class ArgumentParser(argparse.ArgumentParser):
+    def __is_valid_directory(self, parser, arg):
+        if not os.path.isdir(arg):
+            parser.error('Directory {} not found.'.format(arg))
+        else:
+            return arg
+
+    def add_argument_with_dir_check(self, *args, **kwargs):
+        kwargs['type'] = lambda x: self.__is_valid_directory(self, x)
+        self.add_argument(*args, **kwargs)
 
 def parse_cli_args():
-    parser = argparse.ArgumentParser(
+    parser = ArgumentParser(
         description="Firetail - An EVE Online Discord Bot")
     parser.add_argument(
         "--no-restart", "-r",
         help="Disables auto-restart.", action="store_true")
     parser.add_argument(
         "--debug", "-d", help="Enabled debug mode.", action="store_true")
+    parser.add_argument_with_dir_check(
+        "--config", help="Directory with config.py file.")
     return parser.parse_known_args()
 
 
@@ -36,10 +49,15 @@ def main():
     if launch_args.debug:
         ft_args.append('-d')
 
+    if launch_args.config:
+        env = dict(os.environ, FTCONFIG=launch_args.config)
+    else:
+        env = os.environ
+
     ft_args.append('-l')
 
     while True:
-        code = subprocess.call(["firetail-bot", *ft_args])
+        code = subprocess.call(["firetail-bot", *ft_args], env=env)
         if code == 0:
             print("Goodbye!")
             break
@@ -52,3 +70,6 @@ def main():
             print("I crashed! Trying to restart...\n")
     print("Exit code: {exit_code}".format(exit_code=code))
     sys.exit(code)
+
+if __name__ == '__main__':
+    main()
