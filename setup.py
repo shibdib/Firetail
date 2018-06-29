fac99574ff9b8f2c8c5f526da09cecbe16f61ec0
Bob Sardinia <shibdib@users.noreply.github.com>
2018-06-29 10:07:32 -0400

    aiohttp

--- setup.py
+++ setup.py
@@ -31,8 +31,7 @@ setup(
         'asyncpg>=0.13',
         'pytz',
         'youtube_dl',
-        'aiohttp>=3.2.0',
-        'feedparser'
+        'aiohttp>=3.3.0,<3.4.0'
     ],
 
     dependency_links=[
